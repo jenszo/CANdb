b0974a1fa0adf8c250ffefbf5272371f65f325ef
jenszo <6693266+jenszo@users.noreply.github.com>
2021-02-05 22:09:17 +0100

    Cosmetics
    Signed-off-by: jenszo <6693266+jenszo@users.noreply.github.com>

--- src/dbcparser.h
+++ src/dbcparser.h
@@ -1,15 +1,15 @@
 #ifndef __CANDB_H
 #define __CANDB_H
 
-// Note: This file should never be published
-// as a public header. CanDbOrError depends on an foos implemnetation
-// of the proposed std::expect - which is nice but forces any consuming
+// Note: This file should never be published for linkage
+// as a public header. CanDbOrError depends on an foss implementation
+// of the proposed std::expected - which is nice but forces any consuming
 // code to pull in and use the very same std::except header. If not - or other
-// dependencies ship with a different variant - , chances are that different versions
-// will eventually break the binary ABI!
+// dependencies may ship with a different variant - chances are that different versions
+// will eventually break the ABI!
 
-// This file is for internal purposes only! For the public API (which exchanges PoD only)
-// include dbc.h
+// This file is for internal purposes only for for compiling in!
+// For the public API (which exchanges PoD only) include dbc.h
 
 #include "parser.hpp"
 
